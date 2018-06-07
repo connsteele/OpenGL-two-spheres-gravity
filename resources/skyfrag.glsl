62db7e9db7e04c897bc778c60520dbffdc83d8f4
connsteele <35544211+connsteele@users.noreply.github.com>
2018-06-07 01:46:09 -0700

    1st

---
+++ resources/skyfrag.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+out vec4 color;
+in vec3 vertex_normal;
+in vec3 vertex_pos;
+in vec2 vertex_tex;
+uniform vec3 campos;
+
+uniform sampler2D tex;
+uniform sampler2D tex2;
+
+void main()
+{
+	vec4 tcol = texture(tex, vertex_tex);
+	color = tcol;
+	color *= 0.4; //make the scene darker
+	color = clamp (color, 0, 1);
+}
